162e0ba001b34a3030dcab4a66469887861a27fe
Chastagnac <leochastagnac@gmail.com>
2022-11-15 08:53:38 +0100

    update moe page

--- src/views/Moe/Moe.vue
+++ src/views/Moe/Moe.vue
@@ -1,17 +1,6 @@
 <template>
   <div>
     <h1 class="white--text text-center mt-10 mb-10">Partie : MOE</h1>
-    <!-- <v-card class="mx-auto mb-15" max-width="1000" elevation="8" shaped outlined>
-      <h4 class="overline text-decoration-underline mt-10 mb-3 ml-15">Contexte</h4>
-      <p class="body-2 mb-5 ml-15 mr-15">
-        Depuis toujours l’entreprise ne possède qu’un seul développeur. Néanmoins, company a recruté deux nouveaux
-        développeurs n’ayant pas d’expérience dans la programmation à plusieurs.
-      </p>
-      <p class="body-2 mb-5 ml-15 mr-15">
-        Un problème se pose, comment travailler ensemble sur le projet afin d’augmenter la productivité ? Essayez de lui
-        fournir un moyen de coder à plusieurs
-      </p>
-    </v-card> -->
     <v-row align="center" justify="center">
       <v-stepper v-model="e6" vertical width="1000" elevation="8" shaped outlined>
         <!-- Context 1/5 -->
@@ -20,21 +9,24 @@
           <small>1/5</small>
         </v-stepper-step>
         <v-stepper-content step="1">
-          <h4 class="overline text-decoration-underline mt-10 mb-3 ml-5">Contexte</h4>
           <p class="body-2 mb-5 ml-5 mr-5">
-            Depuis toujours l’entreprise ne possède qu’un seul développeur. Néanmoins, company a recruté deux nouveaux
+            Depuis toujours l’entreprise ne possède qu’un seul développeur. Néanmoins, AigriCompany a recruté deux nouveaux
             développeurs n’ayant pas d’expérience dans la programmation à plusieurs.
           </p>
           <p class="body-2 mb-5 ml-5 mr-5">
             Un problème se pose, comment travailler ensemble sur le projet afin d’augmenter la productivité ? Essayez de
             lui fournir un moyen de coder à plusieurs
           </p>
-          <v-btn color="primary" @click="e6 = 2"> Continue </v-btn>
-          <v-btn text> Cancel </v-btn>
+          <v-row align="center" justify="center" class="mb-2">
+            <v-btn class="rounded-xl mr-2" @click="e6 = 2"> Coder sur un même ordinateur </v-btn>
+            <v-btn class="rounded-xl ml-2"> Utiliser git </v-btn>
+          </v-row>
         </v-stepper-content>
         <!-- Context 2/5 -->
-        <v-stepper-step :complete="e6 > 2" step="2"> Versioning </v-stepper-step>
-
+        <v-stepper-step :complete="e6 > 2" step="2">
+          Versioning
+          <small>2/5</small>
+        </v-stepper-step>
         <v-stepper-content step="2">
           <p class="body-2 mb-5 ml-5 mr-5">
             DMhh… Coder à plusieurs via git semble être une bonne solution, Connaissez-vous le versioning ?
@@ -43,12 +35,16 @@
             C’est un système de contrôle de version qui va gérer les changements apportés à un code source au fil du
             temps. Quelle estr la premère étape ?
           </p>
-          <v-btn color="primary" @click="e6 = 3"> Continue </v-btn>
-          <v-btn text> Cancel </v-btn>
+          <v-row align="center" justify="center" class="mb-2">
+            <v-btn class="rounded-xl mr-2" @click="e6 = 3"> Initialiser un repository git </v-btn>
+            <v-btn class="rounded-xl ml-2"> Faire son premier commit </v-btn>
+          </v-row>
         </v-stepper-content>
 
-        <v-stepper-step :complete="e6 > 3" step="3"> Repository </v-stepper-step>
-
+        <v-stepper-step :complete="e6 > 3" step="3">
+          Repository
+          <small>3/5</small>
+        </v-stepper-step>
         <v-stepper-content step="3">
           <p class="body-2 mb-5 ml-5 mr-5">
             En voilà une bonne idée ! Utilisez git init, vous verrez que git a créé un dossier caché nommé .git qui
@@ -59,23 +55,29 @@
             et faire un git checkout nomDeMaBranche afin de se mettre sur la branche en question Maintenant nous allons
             faire le premier commit ! Quelle commande utiliser selon vous ?
           </p>
-          <v-btn color="primary" @click="e6 = 4"> Continue </v-btn>
-          <v-btn text> Cancel </v-btn>
+          <v-row align="center" justify="center" class="mb-2">
+            <v-btn class="rounded-xl mr-2" @click="e6 = 4"> Git add</v-btn>
+            <v-btn class="rounded-xl ml-2">Git commit</v-btn>
+          </v-row>
         </v-stepper-content>
 
-        <v-stepper-step step="4"> Commit </v-stepper-step>
+        <v-stepper-step step="4">
+          Commit
+          <small>4/5</small>
+        </v-stepper-step>
         <v-stepper-content step="4">
-          <h4 class="overline text-decoration-underline mt-10 mb-3 ml-5">Commit</h4>
           <p class="body-2 mb-5 ml-5 mr-5">
-            Et oui ! Avant même de comit il faut savoir quel fichiers nous voulons pousser vers le répository, utiliser
+            Et oui ! Avant même de comit il faut savoir quel fichiers nous voulons pousser vers le rpository, utiliser
             git add nom_fichier pour prendre en compte la modification du fichier.
           </p>
           <p class="body-2 mb-5 ml-5 mr-5">
             Un problème se pose, comment travailler ensemble sur le projet afin d’augmenter la productivité ? Essayez de
             lui fournir un moyen de coder à plusieurs
           </p>
-          <v-btn color="primary" @click="e6 = 1"> Continue </v-btn>
-          <v-btn text> Cancel </v-btn>
+          <v-row align="center" justify="center" class="mb-2">
+            <v-btn class="rounded-xl mr-2" @click="e6 = 5"> Coder sur un même ordinateur </v-btn>
+            <v-btn class="rounded-xl ml-2"> Utiliser git </v-btn>
+          </v-row>
         </v-stepper-content>
       </v-stepper>
     </v-row>
