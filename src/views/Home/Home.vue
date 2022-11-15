93ccd05617cef09b4bb372badd89ecfbda72f0d9
foufure13 <florentpat@free.fr>
2022-11-15 14:13:35 +0100

    [style]add hover on cards

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -16,30 +16,30 @@
       Choisis ta thématique 👇
     </h1>
     <v-row class="mt-5" align="center" justify="center">
-      <v-card
-        class="mr-15 ml-15 mb-15 rounded-xl elevation-15"
-        max-width="400"
-        v-for="t in thematic"
-        :key="t.title"
-      >
-        <v-img class="white--text align-end" height="200px" :src="t.image">
-          <v-row class="mb-1" align="center" justify="center">
-            <v-card-title>{{ t.title }}</v-card-title>
+        <v-card
+          class="mr-15 ml-15 mb-15 rounded-xl elevation-15 cardsTheme"
+          max-width="400"
+          v-for="t in thematic"
+          :key="t.title"
+        >
+          <v-img class="white--text align-end" height="200px" :src="t.image">
+            <v-row class="mb-1" align="center" justify="center">
+              <v-card-title>{{ t.title }}</v-card-title>
+            </v-row>
+          </v-img>
+          <v-card-text class="text--primary">
+            <div class="text-center">
+              {{ t.description }}
+            </div>
+          </v-card-text>
+          <v-row justify="center" align="center">
+            <v-card-actions>
+              <v-btn class="rounded-xl mb-5" color="pink" text>
+                Choisir ce thème
+              </v-btn>
+            </v-card-actions>
           </v-row>
-        </v-img>
-        <v-card-text class="text--primary">
-          <div class="text-center">
-            {{ t.description }}
-          </div>
-        </v-card-text>
-        <v-row justify="center" align="center">
-          <v-card-actions>
-            <v-btn class="rounded-xl mb-5" color="pink" text>
-              Choisir ce thème
-            </v-btn>
-          </v-card-actions>
-        </v-row>
-      </v-card>
+        </v-card>
     </v-row>
   </div>
 </template>
