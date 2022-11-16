3bbbaf5639166df17915c799139c5e35cb7c1f70
Gautier  Luisin <gautierluisin@gmail.com>
2022-11-16 15:09:17 +0100

    [Rh] Fix unsuccessful

--- src/views/Unsuccessful/Unsuccessful.vue
+++ src/views/Unsuccessful/Unsuccessful.vue
@@ -1,20 +1,19 @@
 <template>
   <div>
-    <h1>Vous êtes licencié !</h1>
-    <h2>Vous avez reçu 3 avertissements...</h2>
+    <h1>Vous avez reçu 3 avertissements</h1>
     <v-row align="center" justify="center">
-      <img id="manager" src="@/assets/manager.png" />
+      <img id="manager" src="@/assets/game-over.png" />
     </v-row>
     <v-row align="center" justify="center">
       <v-btn
-        class="rounded-lg mt-10 mb-15 elevation-1"
-        color="white"
-        to="/"
-        light
-        outlined
-        depressed
-        x-large
-        >Recommencer</v-btn
+          class="rounded-lg mt-10 mb-15 elevation-1"
+          color="white"
+          to="/"
+          light
+          outlined
+          depressed
+          x-large
+      >Recommencer</v-btn
       >
     </v-row>
   </div>
