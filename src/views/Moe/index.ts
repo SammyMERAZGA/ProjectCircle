515e27689f27d5c0901e0232da7cefc166cdafc3
Chastagnac <leochastagnac@gmail.com>
2022-11-16 11:54:29 +0100

    [MOE] finis moe parts

--- src/views/Moe/index.ts
+++ src/views/Moe/index.ts
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import draggable from "vuedraggable";
 import { Component } from "vue-property-decorator";
-
 @Component({
   components: {
     draggable,
@@ -9,11 +8,17 @@ import { Component } from "vue-property-decorator";
 })
 export default class Moe extends Vue {
   e6 = 1;
-  dialog = false;
   snackbar = false;
+  snackbarTrue = false;
+  snackbarFalse = false;
   game1 = true;
   display = "Functional third party";
   order = 17;
+
+  dialogSanction = false;
+  dialogSuccess = false
+  nbLives = 3;
+
   rows = [
     {
       text: "Liste dans le désordre",
@@ -30,16 +35,6 @@ export default class Moe extends Vue {
         {
           title: "git add",
           id: 3
-        }
-      ]
-    },
-    {
-      text: "Liste à mettre dans l'ordre",
-      index: 2,
-      items: [
-        {
-          title: "git commit -m",
-          id: 4
         },
         {
           title: "git push origin",
@@ -50,6 +45,16 @@ export default class Moe extends Vue {
           id: 6
         }
       ]
+    },
+    {
+      text: "Liste à mettre dans l'ordre",
+      index: 2,
+      items: [
+        {
+          title: "git commit -m",
+          id: 4
+        }
+      ]
     }
   ]
   computed(): void {
@@ -58,13 +63,39 @@ export default class Moe extends Vue {
       alert("fini");
     }
   }
+  removeLife() {
+    this.nbLives--;
+    this.snackbarFalse = true;
+    if (this.nbLives === 0) {
+      this.snackbarFalse = false;
+      this.dialogSanction = true;
+    }
+  }
+  
+  checkResult() {
+    let itemsIdx = 1
+    let error = false
+    for (let i = 0; i < this.rows[1].items.length; i++) {
+      if (this.rows[1].items[i].id !== itemsIdx) {
+        error = true
+      }
+      itemsIdx++
+    }
+    if (error && this.nbLives > 0) {
+      this.snackbarFalse = false
+      this.removeLife()
+    } else {
+      this.dialogSuccess = true
+    }
+  }
   changeCard(numCard: number, valide: boolean) {
     this.e6 = numCard;
 
     if (valide === false) {
-      this.dialog = true;
+      this.snackbarFalse = true;
+      this.removeLife()
     } else {
-      this.snackbar = true;
+      this.snackbarTrue = true;
     }
     if (numCard === 6) {
       setTimeout(() => {
