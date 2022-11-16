dc349d19af0c561aee4159e6423f0950a1160ac8
Sammy Merazga <sammy.merazga@gmail.com>
2022-11-16 09:25:22 +0100

    [MOA]

--- src/router/index.ts
+++ src/router/index.ts
@@ -37,7 +37,7 @@ const routes: Array<RouteConfig> = [
     component: Moa,
   },
   {
-    path: "/play/scrum",
+    path: "/play/rh",
     name: "Scrum",
     component: Scrum,
   },
--- src/views/Moa/index.ts
+++ src/views/Moa/index.ts
@@ -5,4 +5,20 @@ import { Component } from "vue-property-decorator";
 export default class Moa extends Vue {
   snackbarTrue = false;
   snackbarFalse = false;
+  
+  dialogGameOver = false;
+
+  nbLives = 3;
+
+  removeLife() {
+    this.nbLives--;
+    this.snackbarFalse = true;
+    if (this.nbLives === 0) {
+      this.snackbarFalse = false;
+      this.dialogGameOver = true;
+      setTimeout(() => {
+        this.$router.push("/home");
+      }, 3000);
+    }
+  }
 }
