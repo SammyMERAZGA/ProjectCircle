275c37582f958abd1e3f717dd6f6e59445f33174
Lehning Merlin <merlin@logilink.fr>
2022-11-18 10:56:04 +0100

    spaces and new rules

--- src/views/Landing/Landing.vue
+++ src/views/Landing/Landing.vue
@@ -81,47 +81,37 @@
                   Règle 1
                 </h4>
                 <p class="body-2 mb-5">
-                  Pour arriver à bout d’une partie de Project nightmare, vous
-                  allez devoir jouer à 4 mini jeux différents. Chacun des mini
-                  jeux représente une situation de crise que l’on peut
-                  rencontrer en entreprise. Votre mission sera de solutionner
-                  chacun des problèmes.
+                  Pour arriver à bout d’une partie de Project nightmare, tu vas devoir jouer à 4 mini jeux différents.
+                  Chacun des mini jeux représentent une situation de crise que l’on peut rencontrer en entreprise.
+                  Ta mission sera de solutionner chacun des problèmes.
                 </p>
                 <h4 class="overline text-decoration-underline mt-3 mb-3">
                   Règle 2
                 </h4>
                 <p class="body-2 mb-5">
-                  Au lancement du jeu, vous pourrez choisir par lequel des 4
-                  pôles de l’entreprise vous souhaitez
-                  commencer. Vous aurez le choix entre le pôle Marketing, le
-                  pôle MOE, le pôle MOA ou bien le pôle Finance.
+                  Au lancement du jeu, tu pourras choisir par lequel des 4 pôles de l’entreprise tu souhaites commencer.
+                  Tu auras le choix entre le pôle RH, le pôle MOE, le pôle MOA ou bien le pôle Finance.
                 </p>
                 <h4 class="overline text-decoration-underline mt-3 mb-3">
                   Règle 3
                 </h4>
                 <p class="body-2 mb-10">
-                  Chaque mini jeu sera introduit par un cours explicatif vous
-                  permettant de découvrir de nouvelles notions sur divers
-                  domaines : gestion de projet, développement…. Une fois le
-                  cours terminé vous devrez mettre en pratique ce que vous avez
-                  découvert au travers d’une énigme.
+                  Chaque mini jeu sera introduit par un cours explicatif te permettant de découvrir de nouvelles notions
+                  sur divers domaines : gestion de projet, développement…
+                  Une fois le cours terminé tu devras mettre en pratique ce que tu as découvert au travers d’une énigme.
                 </p>
                 <h4 class="overline text-decoration-underline mt-3 mb-3">
                   Règle 4
                 </h4>
                 <p class="body-2 mb-10">
-                  Vous ne pourrez pas changer de salles tant que vous n’aurait
-                  pas trouvé la solution présentée dans la salle actuelle où
-                  vous jouez.
+                  Tu ne pourras pas changer de salles tant que tu n'auras pas trouvé la solution présentée dans la salle où tu joues.
                 </p>
                 <h4 class="overline text-decoration-underline mt-3 mb-3">
                   Règle 5
                 </h4>
                 <p class="body-2 mb-10">
-                  Si vous fournissez une mauvaise réponse au cours de l’un des 4
-                  mini jeux, vous aurez un avertissement. Au bout du troisième
-                  avertissement vous seriez licencié et vous devrez recommencer
-                  le jeu depuis le début.
+                  Pour chaque mini jeu tu auras 3 tentatives. Si au bout de ces 3 tentatives tu n'as pas résolu l'énigme alors tu
+                  receveras un avertissement. Au bout de 3 avertissements tu seras licencié et tu devras recommencer le jeu depuis le début.
                 </p>
               </v-container>
             </v-card-text>
