c66f7ee8fbb7732d747b444dc9ef4e05e4ec893c
Gautier  Luisin <gautierluisin@gmail.com>
2022-11-18 11:02:40 +0100

    [All] ADD gif sucessful

--- src/views/Successful/Successful.vue
+++ src/views/Successful/Successful.vue
@@ -5,9 +5,13 @@
     <div class="firework"></div>
     <div class="firework"></div>
 
-    <h1 class="black--text mt-10">Vous avez réussi à redresser l'entreprise !</h1>
+    <h1 class="black--text mt-10 mb-10">Vous avez réussi à redresser l'entreprise !</h1>
     <v-row align="center" justify="center">
-      <img class="rounded-xl elevation-10" id="manager" src="@/assets/development.png" />
+      <v-img
+          class="rounded-xl elevation-10"
+          src="@/assets/gif/GIF_FINAL.gif"
+          max-width="1000"
+      />
     </v-row>
     <v-row align="center" justify="center">
       <v-btn
